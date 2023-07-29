69899ff1ae237f91e681cd3d713747b00e299af8
Aisha <87562264+aishasalim@users.noreply.github.com>
2023-07-29 08:24:48 -0500

    Delete tokens_data.py

--- tokens_data.py
+++
@@ -1,9 +0,0 @@
-spreadsheets_id = "YOUR ID FROM GOOGLE SHEETS API"
-spreadsheets_api_key = "YOUR API KEY FROM GOOGLE SHEETS API"
-sheets_endpoint = f"https://sheets.googleapis.com/v4/spreadsheets" \
-                         f"/{spreadsheets_id}/values/Sheet1?key={spreadsheets_api_key}"
-
-kiwi_endpoint = "https://api.tequila.kiwi.com/v2/search"
-kiwi_token = "YOUR TOKEN FROM KIWI API"
-
-bot_token = "YOUR TOKEN FOR TELEGRAM BOT"
