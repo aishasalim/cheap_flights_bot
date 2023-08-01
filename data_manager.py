04752ae0d955952cea9e80fedcdb649efbae9184
Aisha <87562264+aishasalim@users.noreply.github.com>
2023-08-01 11:01:17 -0500

    data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -2,25 +2,26 @@ import requests
 import datetime
 
 class DataManager:
-    def __init__(self, sheets_endpoint, kiwi_endpoint, kiwi_token):
+    def __init__(self, kiwi_endpoint, kiwi_token, list_of_destinations=None):
+        self.data_kiwi = None
         self.kiwi_endpoint = kiwi_endpoint
         self.kiwi_token = kiwi_token
-
-        self.sheets_respond = requests.get(url=sheets_endpoint)
-        self.sheets_data = self.sheets_respond.json()
-        self.sheet_list = self.sheets_data["values"]
+        self.list_of_destinations = list_of_destinations
 
         self.date_from = datetime.datetime.now().strftime("%d/%m/%Y")
         self.date_to = datetime.datetime.now() + datetime.timedelta(30)
         self.return_from = datetime.datetime.now() + datetime.timedelta(10)
         self.return_to = self.date_to + datetime.timedelta(10)
 
+    def retrieve_iata_code(self, city_name):
+        print(city_name)
+
     def get_info_kiwi(self, fly_to):
-        self.header = {
+        header = {
             "apikey": self.kiwi_token
         }
 
-        self.body = {
+        body = {
             "fly_from": "IAH",
             "fly_to": fly_to,
             "date_from": self.date_from,
@@ -28,9 +29,11 @@ class DataManager:
             "return_from": self.return_from.strftime("%d/%m/%Y"),
             "return_to": self.return_to.strftime("%d/%m/%Y"),
             "adults": 1,
+            "children": 0,
             "selected_cabins": "M",
             "curr": "USD",
-            "max_sector_stopovers": 1
+            "max_sector_stopovers": 1,
+            "nights_in_dst_from": 7,
         }
-        respond = requests.get(self.kiwi_endpoint, params=self.body, headers=self.header)
+        respond = requests.get(self.kiwi_endpoint, params=body, headers=header)
         self.data_kiwi = respond.json()
