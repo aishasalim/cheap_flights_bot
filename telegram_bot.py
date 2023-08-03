12ee3676b48afba3e9860658383c0ac78d5cdcff
Aisha <87562264+aishasalim@users.noreply.github.com>
2023-08-03 11:41:44 -0500

    telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,6 @@ import telebot
 from telebot import types
 from flight_search import FlightSearch
 
-
 class Bot:
     def __init__(self, bot_token, data_manager):
         self.data_manager = data_manager
@@ -10,6 +9,23 @@ class Bot:
         self.current_result_index = 0
         self.result_messages = []
         self.set_handlers()
+
+    def buttons_of_iata(self, chat_id, iata_codes):
+        markup = types.InlineKeyboardMarkup()
+        for code in iata_codes:
+            button_text = f"{code[0]} ({code[1]})"
+            button_callback_data = f"iata:{code[1]}"
+            button = types.InlineKeyboardButton(text=button_text, callback_data=button_callback_data)
+            markup.add(button)
+        self.bot.send_message(chat_id, "Available IATA Codes:", reply_markup=markup)
+
+
+    def handle_city_name(self, message):
+        city_name = message.text
+        iata_codes = self.data_manager.retrieve_iata_code(city_name)
+        self.buttons_of_iata(message.chat.id, iata_codes)
+        self.run_flight_search()
+        self.send_markup(message.chat.id)
 
     def run_flight_search(self):
         list_of_destinations = self.data_manager.list_of_destinations
@@ -19,15 +35,21 @@ class Bot:
     def set_handlers(self):
         @self.bot.message_handler(commands=['start'])
         def startBot(message):
-            welcome_message = "This is a bot that will send you to the cheapest place you want! Type your city name."
+            welcome_message = "This is a bot that will send you on the " \
+                              "CHEAPEST airplane ticket in the nearest month! " \
+                              "✈️ Type your city name and let's explore the " \
+                              "world together! 🌍"
             self.bot.send_message(message.chat.id, welcome_message)
 
         @self.bot.message_handler(func=lambda message: True)
-        def handle_city_name(message):
-            city_name = message.text
-            self.data_manager.retrieve_iata_code(city_name)
-            self.run_flight_search()
-            self.send_markup(message.chat.id)
+        def handle_city_name_message(message):
+            self.handle_city_name(message)
+
+        @self.bot.callback_query_handler(func=lambda call: call.data.startswith('iata:'))
+        def handle_iata_button(function_call):
+            iata_code = function_call.data.split(':')[1]
+            print(f"IATA Code pressed: {iata_code}")
+            self.bot.answer_callback_query(function_call.id)
 
         @self.bot.callback_query_handler(func=lambda call: True)
         def response(function_call):
